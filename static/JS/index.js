de348ae07ab61051dfeca997c9c4ead139e8659c
karuu-01 <brian.muchori@student.moringaschool.com>
2021-06-14 00:26:23 +0300

    add changes

--- static/JS/index.js
+++ static/JS/index.js
@@ -1,9 +1,9 @@
-// Function to refresh the form after submition
+// Function to refresh the form after submition//
 function formRefresh(){
     document.getElementById("form_data").reset();
 }
 
-// Event function on button
+// Event function on button//
 function akanNames(){
 
     var birthDate=document.getElementById("birthdate").value;
@@ -12,7 +12,7 @@ function akanNames(){
     var month=parseInt(dateEntered[1]);
     var year=parseInt(dateEntered[0]);
 
-    //Century calculation
+    //Century calculation//
     var century=(year-1)/100+1; 
     var dayOfTheWeek = ( century/4 -2*century-1 + 5*year/4  + 26*(month+1)/10 + day ) % 7;
         
@@ -23,7 +23,7 @@ function akanNames(){
     var daysOfWeek=["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     var dOfWeek = Math.round(dayOfTheWeek)
 
-    //form validation and output
+    //form validation and output//
     if(!isNaN(dOfWeek)){
         if(gender=="Male"){
             document.getElementById("display").innerHTML= "Your Akan name is " + maleNames[Math.round(dayOfTheWeek)] + " and you were born on " + daysOfWeek[dOfWeek];
